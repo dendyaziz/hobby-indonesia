1c0af8f17d512d77aaccbefa99173833dba5cb81
Dendy <dendy.aziz@gmail.com>
2026-05-25 21:45:05 +0700

    feat: login test case

--- tests/Feature/FilamentTest.php
+++ tests/Feature/FilamentTest.php
@@ -1,7 +1,36 @@
 <?php
 
+use App\Models\User;
+use Filament\Facades\Filament;
+use Filament\Auth\Pages\Login;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Livewire\Livewire;
+
+uses(RefreshDatabase::class);
+
+beforeEach(function () {
+    Filament::setCurrentPanel(Filament::getPanel('admin'));
+});
+
 it('has a login page for filament', function () {
     $response = $this->get('/admin/login');
 
     $response->assertStatus(200);
+});
+
+it('allows registered admin users to log in', function () {
+    $user = User::factory()->create([
+        'email' => 'admin@hobbyindonesia.com',
+        'password' => bcrypt('password'),
+    ]);
+
+    Livewire::test(Login::class)
+        ->fillForm([
+            'email' => $user->email,
+            'password' => 'password',
+        ])
+        ->call('authenticate')
+        ->assertRedirect('/admin');
+
+    $this->assertAuthenticatedAs($user);
 });
