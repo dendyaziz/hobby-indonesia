227dfbeb6fadf106fdbd9aabc371935891fd429d
Dendy <dendy.aziz@gmail.com>
2026-05-26 21:16:00 +0700

    feat: update testimony navigation icon

--- app/Filament/Resources/Testimonies/TestimonyResource.php
+++ app/Filament/Resources/Testimonies/TestimonyResource.php
@@ -18,7 +18,7 @@ class TestimonyResource extends Resource
 {
     protected static ?string $model = Testimony::class;
 
-    protected static string|BackedEnum|null $navigationIcon = Heroicon::OutlinedRectangleStack;
+    protected static string|BackedEnum|null $navigationIcon = Heroicon::OutlinedChatBubbleBottomCenterText;
 
     public static function form(Schema $schema): Schema
     {
