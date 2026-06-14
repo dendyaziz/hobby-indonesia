83452013c6a9a22fb0dc28f49620f5ba0b7e25b3
Dendy <dendy.aziz@gmail.com>
2026-06-14 19:08:28 +0700

    feat: article form group and section

--- app/Filament/Resources/Articles/Schemas/ArticleForm.php
+++ app/Filament/Resources/Articles/Schemas/ArticleForm.php
@@ -8,6 +8,8 @@ use Filament\Forms\Components\RichEditor;
 use Filament\Forms\Components\RichEditor\ToolbarButtonGroup;
 use Filament\Forms\Components\SpatieMediaLibraryFileUpload;
 use Filament\Forms\Components\TextInput;
+use Filament\Schemas\Components\Group;
+use Filament\Schemas\Components\Section;
 use Filament\Schemas\Components\Utilities\Set;
 use Filament\Schemas\Schema;
 use Illuminate\Support\Str;
@@ -17,50 +19,65 @@ class ArticleForm
     public static function configure(Schema $schema): Schema
     {
         return $schema
-            ->columns(1)
+            ->columns(3)
             ->components([
-                TextInput::make('title')
-                    ->required()
-                    ->maxLength(150)
-                    ->live(onBlur: true)
-                    ->afterStateUpdated(fn (string $operation, $state, Set $set) => $operation === 'create' ? $set('slug', Str::slug($state)) : null),
+                Group::make()
+                    ->schema([
+                        Section::make()
+                            ->schema([
+                                TextInput::make('title')
+                                    ->required()
+                                    ->maxLength(150)
+                                    ->live(onBlur: true)
+                                    ->afterStateUpdated(fn(string $operation, $state, Set $set) => $operation === 'create' ? $set('slug', Str::slug($state)) : null),
 
-                TextInput::make('slug')
-                    ->disabled()
-                    ->dehydrated()
-                    ->required()
-                    ->maxLength(255)
-                    ->unique(Article::class, 'slug', ignoreRecord: true),
+                                TextInput::make('slug')
+                                    ->disabled()
+                                    ->dehydrated()
+                                    ->required()
+                                    ->maxLength(255)
+                                    ->unique(Article::class, 'slug', ignoreRecord: true),
 
-                SpatieMediaLibraryFileUpload::make('image')
-                    ->collection('featured_images')
-                    ->image()
-                    ->imageEditor()
-                    ->imagePreviewHeight(250)
-                    ->required(),
+                                SpatieMediaLibraryFileUpload::make('image')
+                                    ->collection('featured_images')
+                                    ->image()
+                                    ->imageEditor()
+                                    ->imagePreviewHeight(250)
+                                    ->required(),
 
-                RichEditor::make('content')
-                    ->required()
-                    ->toolbarButtons([
-                        ['bold', 'italic', 'underline', 'link', 'attachFiles'],
-                        [ToolbarButtonGroup::make('Paragraph', ['paragraph', 'h2', 'h3'])],
-                        [ToolbarButtonGroup::make('Alignment', ['alignStart', 'alignCenter', 'alignEnd', 'alignJustify'])],
-                        ['blockquote', 'bulletList', 'orderedList'],
-                        ['undo', 'redo'],
+                                RichEditor::make('content')
+                                    ->required()
+                                    ->toolbarButtons([
+                                        ['bold', 'italic', 'underline', 'link', 'attachFiles'],
+                                        [ToolbarButtonGroup::make('Paragraph', ['paragraph', 'h2', 'h3'])],
+                                        [ToolbarButtonGroup::make('Alignment', ['alignStart', 'alignCenter', 'alignEnd', 'alignJustify'])],
+                                        ['blockquote', 'bulletList', 'orderedList'],
+                                        ['undo', 'redo'],
+                                    ])
+                                    ->fileAttachmentsDirectory('public/articles/attachments')
+                                    ->fileAttachmentsMaxSize(2048)
+                                    ->resizableImages()
+                                    ->preventFileAttachmentPathTampering()
+                                    ->columnSpanFull(),
+                            ]),
                     ])
-                    ->fileAttachmentsDirectory('public/articles/attachments')
-                    ->fileAttachmentsMaxSize(2048)
-                    ->resizableImages()
-                    ->preventFileAttachmentPathTampering()
-                    ->columnSpanFull(),
+                    ->columnSpan(['lg' => 2]),
 
-                Radio::make('status')
-                    ->options([
-                        'draft' => 'Draft',
-                        'published' => 'Published',
+                Group::make()
+                    ->schema([
+                        Section::make('Publish')
+                            ->schema([
+
+                                Radio::make('status')
+                                    ->options([
+                                        'draft' => 'Draft',
+                                        'published' => 'Published',
+                                    ])
+                                    ->required()
+                                    ->default('draft'),
+                            ]),
                     ])
-                    ->required()
-                    ->default('draft'),
+                    ->columnSpan(['lg' => 1]),
             ]);
     }
 }
